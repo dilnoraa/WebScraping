5f27b328eb244b6a4ca660e497250118bd3980e1
Dilnora Matniyazova <dilnora.matniyazova@gmail.com>
2020-05-13 23:13:30 +0200

    Update createFileForUrls.java

--- WebScraping/JavaApplication12/src/Project/createFileForUrls.java
+++ WebScraping/JavaApplication12/src/Project/createFileForUrls.java
@@ -1,4 +1,3 @@
-
 package Project;
 
 import java.io.File;
@@ -8,30 +7,29 @@ import java.io.PrintWriter;
 
 
 public class createFileForUrls {
-    
-    public static void createFile()
-    {
-          String fileName = "FileOfUrls.txt";
-           PrintWriter printWriter = null;
-           File file = new File(fileName);
-       try {
-       
+
+    public static void createFile() {
+        String fileName = "FileOfUrls.txt";
+        PrintWriter printWriter = null;
+        File file = new File(fileName);
+        try {
+
             file.createNewFile();
-        printWriter = new PrintWriter(new FileOutputStream(fileName, false));
-     
-        printWriter.write("http://htmlunit.sourceforge.net/ \n"); //  every line has only one URL string, so I add "\n"
-        printWriter.write("https://netbeans.org/features/java/ \n");
-        printWriter.write("https://www.java.com/en/download/faq/whatis_java.xml");
-                
-         } catch (IOException e) {
-           System.out.println(e);
-    } finally {
-        if (printWriter != null) {
-            printWriter.flush();
-            printWriter.close();
+            printWriter = new PrintWriter(new FileOutputStream(fileName, false));
+
+            printWriter.write("http://htmlunit.sourceforge.net/ \n");
+            printWriter.write("https://netbeans.org/features/java/ \n");
+            printWriter.write("https://www.java.com/en/download/faq/whatis_java.xml");
+
+        } catch (IOException e) {
+            System.out.println(e);
+        } finally {
+            if (printWriter != null) {
+                printWriter.flush();
+                printWriter.close();
+            }
         }
+
     }
-        
-    }
-    
+
 }
